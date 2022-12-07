7c6acc5c315f4aa8fbdf13abf43d53592995608a
Clemence Llovio <cllovio@z2r3p7.42lyon.fr>
2022-12-07 13:59:19 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cllovio <cllovio@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 11:06:34 by cllovio           #+#    #+#             */
+/*   Updated: 2022/12/07 11:42:05 by cllovio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//get_next_line.c
+char	*get_next_line(int fd);
+
+//get_next_line_utils.c
+#endif
