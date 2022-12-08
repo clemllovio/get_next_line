355f22993ebb6c989c6ec33247a42892b5cbe676
Clemence Llovio <cllovio@z3r11p5.42lyon.fr>
2022-12-08 15:28:46 +0100

    first test

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,19 @@
 /*   By: cllovio <cllovio@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 11:06:34 by cllovio           #+#    #+#             */
-/*   Updated: 2022/12/07 11:42:05 by cllovio          ###   ########.fr       */
+/*   Updated: 2022/12/08 12:51:24 by cllovio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFSIZE 10
+
+# include <unistd.h>
+
+// ENLEVER AVANT CORRECTION
+# include <fcntl.h>
+# include <stdio.h>
 
 //get_next_line.c
 char	*get_next_line(int fd);
--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cllovio <cllovio@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 11:04:27 by cllovio           #+#    #+#             */
-/*   Updated: 2022/12/08 14:58:39 by cllovio          ###   ########.fr       */
+/*   Created: 2022/12/08 10:35:57 by cllovio           #+#    #+#             */
+/*   Updated: 2022/12/08 14:58:31 by cllovio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	main(void)
 {
-	static char	buf[BUFSIZE + 1];
-	int			result;
+	int		fd;
+	int		i;
 
-	result = read(fd, buf, BUFSIZE);
-	if (result == -1 || result == 0)
-		return (NULL);
-	return ("ok");
+	fd = open("text.txt", O_RDONLY);
+	i = 0;
+	while (i <= 6)
+	{
+		printf("%s\n", get_next_line(fd));
+		i++;
+	}
+	return (0);
 }
